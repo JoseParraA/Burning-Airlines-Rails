38da61f34f3dfe6b52dacd60c6ad38a7ea8d7914
Taryn <taryn@sugarpush.com.au>
2018-04-19 12:33:50 +1000

    delete migration for plane id

--- db/migrate/20180417095920_change_planeid_column_in_airplanes.rb
+++
@@ -1,7 +0,0 @@
-class ChangePlaneidColumnInAirplanes < ActiveRecord::Migration[5.1]
-
-  def change
-  rename_column :flights, :plane_id, :airplane_id
-  end
-
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,14 @@ u1 = User.create :name => 'admin', :admin => true
 u2 = User.create :name => 'Sam'
 
 Flight.destroy_all
-f1 = Flight.create :airplane_id => '1', :origin => 'Sydney', :destination => 'Brisbane', :date => '2018-04-21'
+f1 = Flight.create :airplane_id => '1', :origin => 'Sydney', :destination => 'Brisbane', :date => 'Fri 20th April 2018'
+f1 = Flight.create :airplane_id => '1', :origin => 'Brisbane', :destination => 'Adelaide', :date => 'Sat 21st April 2018'
+f1 = Flight.create :airplane_id => '1', :origin => 'Adelaide', :destination => 'Sydney', :date => 'Fri 20th April 2018'
+f1 = Flight.create :airplane_id => '1', :origin => 'Sydney', :destination => 'Adelaide', :date => 'Sat 21st April 2018'
+f1 = Flight.create :airplane_id => '2', :origin => 'Adelaide', :destination => 'Brisbane', :date => 'Fri 20th April 2018'
+f1 = Flight.create :airplane_id => '2', :origin => 'Brisbane', :destination => 'Sydney', :date => 'Thurs 19th April 2018'
+f1 = Flight.create :airplane_id => '2', :origin => 'Sydney', :destination => 'Adelaide', :date => 'Thurs 19th April 2018'
+f1 = Flight.create :airplane_id => '2', :origin => 'Bribane', :destination => 'Sydney', :date => 'Sat 21st April 2018'
 
 Airplane.destroy_all
 a1 = Airplane.create :name => '747', :columns => 20, :rows => 4
